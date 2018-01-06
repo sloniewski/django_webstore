a48a8b3ae6c4a84ef36909ea1ec97448dbc0b000
sloniewski <sloniewski.maciej@gmail.com>
2018-01-06 21:33:13 +0100

    script for handling ajax requests adding product to cart

--- core/static/scripts/product-page.js
+++ core/static/scripts/product-page.js
@@ -1,8 +1,34 @@
 $( document ).ready( function(){
+
+	var csrftoken = getCookie('csrftoken');
+
+	$.ajaxSetup({
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+
 	$('#add-item-form').on('submit', function(event) {
 		event.preventDefault();
 		$('#cart-item-count').text(function(event){
-			return $('#item-quantity').val();
+			var form_action_url = $('#add-item-form').attr('action');
+			var item_num = $('#product-id').val();
+			var item_qty = $('#item-quantity').val();
+			$.ajax({
+				url: form_action_url,
+				method: 'POST',
+				data: {
+					item: item_num,
+					qty: item_qty
+				}
+			}).done(function (data) {
+				consoe.log(data);
+				return item_qty;
+			}).fail(function() {
+				return 0;
+			});
 		});
 	});
 });

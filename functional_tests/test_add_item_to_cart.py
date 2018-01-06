8700f6bb7b51e9303ae0d5743032349ca03826d7
sloniewski <sloniewski.maciej@gmail.com>
2018-01-06 19:23:54 +0100

    fixed test and added template for testing

--- functional_tests/test_add_item_to_cart.py
+++ functional_tests/test_add_item_to_cart.py
@@ -1,6 +1,6 @@
-import time
-
 from .base import FunctionalTest
+
+from product.models import Product
 
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -8,6 +8,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 class AddItemToCartTest(FunctionalTest):
 
     def test_add_item_to_cart(self):
+        product = Product.objects.create(name='my-test-product')
+        self.browser.get('http://localhost:8000/product/{}/'.format(product.slug))
+
         button = self.browser.find_element_by_id('add-to-cart')
         quantity_box = self.browser.find_element_by_id('item-quantity')
 
@@ -18,6 +21,6 @@ class AddItemToCartTest(FunctionalTest):
                 timeout=2,
                 poll_frequency=0.5
             ).until(
-                    method=lambda x: x.find_element_by_id('cart-items-count').text == '2',
+                    method=lambda x: x.find_element_by_id('cart-item-count').text == '2',
                     message='Item not added to car in 2 sec time'
                 )

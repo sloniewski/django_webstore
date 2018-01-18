6fc32fdbdad53ee747025828da9cb13b08d01304
sloniewski <sloniewski.maciej@gmail.com>
2018-01-18 22:19:53 +0100

    created cash field

--- webstore/cash/db/fields.py
+++ webstore/cash/db/fields.py
@@ -5,15 +5,25 @@ from .cash import Cash
 
 
 class CashField(CharField):
-    
+    max_length = 12
 
-    def to_pyhon(self, value):
+    def parse_cash(self, value):
+        try:
+            value = Cash(value)
+        except ValueError:
+            raise ValidationError
+        return value
+
+    def from_db_value(self, value, expression=None, connection=None):
+        if value is None:
+            return value
+        return self.parse_cash(value)
+
+    def to_python(self, value):
         value = super().to_python(value)
         if value in self.empty_values:
             return None
-        if not isinstance(value, Cash):
-            try:
-                value = Cash(value)
-            except ValueError:
-                raise ValidationError(self.error_messages['invalid'], code='invalid')
-            return value
+        return self.parse_cash(value)
+
+    def get_prep_value(self, value):
+        return str(value)

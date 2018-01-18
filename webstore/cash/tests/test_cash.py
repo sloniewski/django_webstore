101c794fd4f41949165f925890e8aebaeb70d1b8
sloniewski <sloniewski.maciej@gmail.com>
2018-01-18 22:18:56 +0100

    tests for CashField

--- webstore/cash/tests/test_cash.py
+++ webstore/cash/tests/test_cash.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from webstore.cash.db.cash import Cash
+from webstore.cash.db.fields import CashField
 
 
 class TestCash(TestCase):
@@ -8,3 +9,23 @@ class TestCash(TestCase):
     def test_currency(self):
         cash = Cash()
         self.assertEqual(cash.currency, 'EUR')
+
+
+class TestCashField(TestCase):
+
+    def test_to_python_returns_cash_obj(self):
+        field = CashField()
+        obj = field.to_python('1.1')
+        self.assertEqual(obj, Cash('1.1'))
+        self.assertIsInstance(obj, Cash)
+
+    def test_from_db_value_returns_cash_obj(self):
+        field = CashField()
+        obj = field.from_db_value('1.1')
+        self.assertEqual(obj, Cash('1.1'))
+        self.assertIsInstance(obj, Cash)
+
+    def test_get_prep_value_returns_str(self):
+        field = CashField()
+        value = field.get_prep_value(Cash('2.2'))
+        self.assertEqual(value, '2.2')

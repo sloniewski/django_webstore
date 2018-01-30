e1e6a88f7d30e8cdfda03a4f8ca85927b4c3e10a
sloniewski <sloniewski.maciej@gmail.com>
2018-01-30 23:10:18 +0100

    fixed broken functional test

--- webstore/functional_tests/test_cart_shows_summary.py
+++ webstore/functional_tests/test_cart_shows_summary.py
@@ -2,6 +2,8 @@ from webstore.cart.models import Cart
 
 from .base import FunctionalTest
 
+import time
+
 
 class TestCartShowsSummary(FunctionalTest):
 
@@ -14,7 +16,7 @@ class TestCartShowsSummary(FunctionalTest):
         cart.add_item(item=prod_a.id, qty=2)
         cart.add_item(item=prod_b.id, qty=5)
 
-        url = self.live_server_url + '/cart/'
+        url = self.live_server_url + '/cart/list-item/'
         self.browser.get(url)
 
         self.browser.add_cookie({
@@ -25,9 +27,9 @@ class TestCartShowsSummary(FunctionalTest):
 
         self.browser.refresh()
 
-        cart_items_list = self.browser.find_elements_by_class_name('cart-list-item')
+        cart_items_list = [x.text for x in self.browser.find_elements_by_class_name('cart-list-item')]
         self.assertIn(prod_a.name, cart_items_list)
         self.assertIn(prod_b.name, cart_items_list)
 
-        cart_summary = self.browser.find_element_by_id('cart-summary')
+        cart_summary = self.browser.find_element_by_id('cart-total-value')
         self.assertEqual(cart_summary.text, '18.99')

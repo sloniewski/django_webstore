6c64e2cdc6489b789db300125aeeb8d317cd17a4
sloniewski <sloniewski.maciej@gmail.com>
2018-02-04 14:06:52 +0100

    missing test for create order url

--- webstore/order/tests/test_urls.py
+++ webstore/order/tests/test_urls.py
@@ -2,7 +2,7 @@ from django.urls import reverse, resolve
 from django.test import TestCase
 
 
-class TestOrderUrls(TestCase):
+class TestOrderDetailUrl(TestCase):
 
     def test_reverse(self):
         self.assertEqual(
@@ -14,5 +14,21 @@ class TestOrderUrls(TestCase):
         resolver = resolve(reverse('order:order-detail', kwargs={'pk': 1}))
         self.assertEqual((), resolver.args)
         self.assertEqual({'pk': 1}, resolver.kwargs)
+        self.assertEqual('order', resolver.app_name)
+        self.assertEqual('order', resolver.namespace)
+
+
+class TestOrderCreateUrl(TestCase):
+
+    def test_reverse(self):
+        self.assertEqual(
+            first='/order/create/',
+            second=reverse('order:order-create', kwargs={})
+        )
+
+    def test_resolve(self):
+        resolver = resolve(reverse('order:order-create', kwargs={}))
+        self.assertEqual((), resolver.args)
+        self.assertEqual({}, resolver.kwargs)
         self.assertEqual('order', resolver.app_name)
         self.assertEqual('order', resolver.namespace)

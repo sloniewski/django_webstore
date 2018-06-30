c8ca6ce272d94b670ef544a0bdfacd64f042d995
sloniewski <sloniewski.maciej@gmail.com>
2018-07-01 00:36:19 +0200

    added materialize and styling

--- webstore/cart/context_processors.py
+++ webstore/cart/context_processors.py
@@ -3,10 +3,12 @@ from .models import Cart
 
 def cart_info(request):
     session_id = request.session.session_key
-    cart_item_count = 0
     if session_id is not None:
         cart = Cart.objects.get_or_create(session_id=session_id)[0]
         cart_item_count = cart.get_item_count()
+
+    if cart_item_count is None:
+        cart_item_count = 0
 
     return {
         "cart_items": cart_item_count,
--- webstore/core/static/scripts/product-page.js
+++ webstore/core/static/scripts/product-page.js
@@ -1,15 +1,5 @@
 $( document ).ready( function(){
 
-	var csrftoken = getCookie('csrftoken');
-
-	$.ajaxSetup({
-	    beforeSend: function(xhr, settings) {
-	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-	        }
-	    }
-	});
-
 	$('#add-item-form').on('submit', function(event) {
 		event.preventDefault();
 		$('#cart-item-count').text(function(event){

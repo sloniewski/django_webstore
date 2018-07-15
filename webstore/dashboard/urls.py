d3afe594e6fbbb3252695853a1e66a12530ec91c
sloniewski <sloniewski.maciej@gmail.com>
2018-07-15 23:44:50 +0200

    crud operations for product dashboard

--- webstore/dashboard/urls.py
+++ webstore/dashboard/urls.py
@@ -7,7 +7,19 @@ app_name = 'dashboard'
 
 urlpatterns = [
 
+    path('product/list', views.ProductListView.as_view(),
+         name='product-list'),
+
+    path('product/update/<int:pk>', views.ProductUpdateView.as_view(),
+         name='product-update'),
+
+    path('product/delete/<int:pk>', views.ProductDeleteView.as_view(),
+         name='product-delete'),
+
+    path('product/create', views.ProductCreateView.as_view(),
+         name='product-create'),
+
     path('', views.DashboardWelcomeView.as_view(),
-         name='dashboard-main'),
+             name='dashboard-main'),
 
 ]
--- webstore/dashboard/views.py
+++ webstore/dashboard/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.shortcuts import reverse
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
 
+from webstore.product.models import Product
 
 class DashboardWelcomeView(TemplateView):
     """
@@ -8,3 +16,38 @@ class DashboardWelcomeView(TemplateView):
     :template:`dasboard/base_dashboard.html`
     """
     template_name = 'dashboard/base_dashboard.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'dashboard/product/product_create.html'
+    fields = [
+        'name',
+        'slug',
+        'description',
+        'weight',
+    ]
+
+    def get_success_url(self):
+        return reverse('dashboard:product-list')
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'dashboard/product/product_list.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'dashboard/product/product_update.html'
+    fields = [
+        'name',
+        'slug',
+        'description',
+        'weight',
+    ]
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'dashboard/product/product_delete.html'

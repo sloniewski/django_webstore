2b635ba3d87ceb254a3890dd7ede388e35ed8c9a
sloniewski <sloniewski.maciej@gmail.com>
2018-08-25 00:59:52 +0200

    some refactoring

--- webstore/core/static/scripts/product-page.js
+++ webstore/core/static/scripts/product-page.js
@@ -2,23 +2,23 @@ $( document ).ready( function(){
 
 	$('#add-item-form').on('submit', function(event) {
 		event.preventDefault();
-		$('#cart_item_count').text(function(event){
-			var form_action_url = $('#add-item-form').attr('action');
-			var item_num = $('#product-id').val();
-			var item_qty = $('#item-quantity').val();
-			$.ajax({
-				url: form_action_url,
-				method: 'POST',
-				data: {
-					item: item_num,
-					qty: item_qty
-				}
-			}).done(function (data) {
-				data = JSON.parse(data);
-				$('#cart-item-count').text(data.cart_items);
-			}).fail(function() {
-				return 0;
-			});
+
+		var form_action_url = $('#add-item-form').attr('action');
+		var item_num = $('#product-id').val();
+		var item_qty = $('#item-quantity').val();
+		$.ajax({
+			url: form_action_url,
+			method: 'POST',
+			data: {
+				item: item_num,
+				qty: item_qty
+			}
+		}).done(function (data) {
+			data = JSON.parse(data);
+			$('#cart_item_count').text(data.cart_items);
+		}).fail(function() {
+			return 0;
 		});
+
 	});
 });

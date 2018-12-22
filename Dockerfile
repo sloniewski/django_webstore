4fd70dcefc92c1f9d9c4b314739c3076832a35b1
sloniewski <sloniewski.maciej@gmail.com>
2018-12-22 01:56:03 +0100

    added redis to docker

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:latest
 
 RUN mkdir /home/project
 WORKDIR /home/project
-ADD . /home/project
+ADD requirements.txt /home/project/requirements.txt
 RUN pip install -r requirements.txt
 
 EXPOSE 8000:8000
--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,3 @@
-# THIS !!
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,14 +24,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 MEDIA_URL = 'files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'webstore/core/media')

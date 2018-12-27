6f607122c301c51a26d1a45e18b33f38ea4cbc82
sloniewski <sloniewski.maciej@gmail.com>
2018-12-27 19:19:24 +0100

    tests for delivery panel

--- dashboard/delivery_panel/tests.py
+++ dashboard/delivery_panel/tests.py
@@ -145,5 +145,39 @@ class TestDeliveryViews(TestCase):
             template_name='dashboard/delivery/delivery_option_update.html',
         )
 
+    def test_delivery_price_create_post(self):
+        data = {
+            'name': 'test_name',
+            'cost': 11.37,
+            'active': 'on',
+            'max_weight': 20,
+        }
+        response = self.client.post(
+            reverse(
+                viewname='delivery_panel:delivery-option-create',
+            ),
+            data=urlencode(data),
+            content_type="application/x-www-form-urlencoded",
+        )
+        self.assertEqual(
+            first=response.status_code,
+            second=302,
+            msg='view returned {} code'.format(response.status_code),
+        )
+        self.assertEqual(
+            first=response.url,
+            second=reverse('delivery_panel:delivery-option-list')
+        )
+
+        delivery_option = DeliveryPricing.objects.get(
+            name='test_name',
+            max_weight=20,
+            active=True,
+        )
+        self.assertEqual(delivery_option.name, 'test_name')
+        self.assertEqual(delivery_option.max_weight, 20)
+        self.assertEqual(delivery_option.active, True)
+        self.assertEqual(delivery_option.cost, Decimal('11.37'))
+
 
 
